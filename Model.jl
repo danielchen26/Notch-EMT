38705fe882593eaef9c662a2c0bcf7e105626e0b
Danielchen26 <chentianchi@gmail.com>
2021-01-18 18:19:42 -0500

    start

--- Model.jl
+++ Model.jl
@@ -1,1 +1,10 @@
-using DifferentialEquations
+using DifferentialEquations, DiffEqBiological
+# Building models for Histone + RBPJ with 
+
+EMT_fb = = @reaction_network begin
+ (α, ϵ),            RBPJ+ + Di ↔ Da
+  β,                Da → Da + O
+  η,                Di → Di + O
+ (γ,θ),             Di ↔ Dm
+  δ,                O → ∅
+end α ϵ β η γ θ δ 
